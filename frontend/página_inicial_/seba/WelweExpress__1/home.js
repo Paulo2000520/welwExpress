eaddcd44963d360b6b9282b7c80123845808092c
Paulo <manuel955599704@gmail.com>
2026-05-22 06:11:44 +0100

    Delete frontend/página_inicial_/seba/WelweExpress__1 directory

--- frontend/página_inicial_/seba/WelweExpress__1/home.js
+++
@@ -1,5 +0,0 @@
-let x = Number(document.getElementById("onda").textContent)
-
-document.getElementById("submit").addEventListener("click", () => {
-  document.getElementById("onda").innerHTML = x++
-})
